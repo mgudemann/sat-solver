c212bf7c70c6d137a4b4355bbac9171a2f952b61
Matthias Güdemann <matthias.guedemann@hm.edu>
2022-01-15 11:18:42 +0100

    Convert to unit test
    Test used fixed input to property

--- test/AlgorithmSpec.hs
+++ test/AlgorithmSpec.hs
@@ -15,7 +15,12 @@ spec :: Spec
 spec = do
     describe "Compare with picoSAT solver" $ do
         it "compare SAT / UNSAT result term reduced to empty list" $ do
-          prop_picoSATcomparison [[NonZero 1, NonZero 2],[NonZero (-1)]]
+          let problem = [[1, 2], [- 1]]
+              csol = cdcl (map (map fromIntegral) problem) False False False
+          psol <- PicoSAT.solve problem
+          (case (psol, csol) of
+             (PicoSAT.Solution _, SAT) -> True
+             _                          -> False) `shouldBe` True
         it "compare SAT / UNSAT results" $ do
           property $ \clauses -> prop_picoSATcomparison clauses
 
